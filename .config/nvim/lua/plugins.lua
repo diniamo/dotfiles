db4da684fd2477f39d5bf2522dee30eef9a887b2
diniamo <diniamo69@gmail.com>
2022-08-17 15:13:15 +0200

    remove packer_compilied.lua

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -95,7 +95,7 @@ return require('packer').startup(function(use)
 		requires = 'kevinhwang91/promise-async',
 		config = function()
 			-- Folding
-			vim.o.foldcolumn = '1'
+			-- vim.o.foldcolumn = '1'
 			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 			vim.o.foldlevelstart = 99
 			vim.o.foldenable = true
