c7397c7b25ec63d1b84311b3bb5a4f4563de4b90
diniamo <diniamo69@gmail.com>
2023-06-26 21:44:40 +0200

    add hdmi_toggle script

---
+++ .config/qtile/hdmi_toggle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export PATH=/usr/bin
+
+if [[ $(</sys/class/drm/card0/card0-HDMI-A-1/status) == "connected" ]]
+then
+	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo
+	xrandr --output HDMI-1 --auto && xrandr --output eDP-1 --off
+else
+	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo
+	xrandr --output eDP-1 --auto && xrandr --output HDMI-1 --off
+fi
