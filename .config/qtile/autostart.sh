7910c8a3bdd1d08e537c851b9a020e5d357c2cab
diniamo <diniamo69@gmail.com>
2023-07-31 21:41:08 +0200

    move playerctl pause to xss-lock

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -6,4 +6,4 @@ dunst &
 if [[ -d "/sys/class/power_supply" ]]; then x-on-resize --config ~/.config/qtile/hdmi_toggle.sh & fi
 redshift -l 47.1625:19.5033 &
 betterlockscreen -u $WALLPAPER
-xss-lock -- betterlockscreen -l dimblur &
+xss-lock -- playerctl pause; betterlockscreen -l dimblur &
