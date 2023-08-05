bfbf6d5609161db4e992b786adda2a8003e62153
diniamo <diniamo69@gmail.com>
2023-08-05 16:37:19 +0200

    rework mpv update_scripts.sh

--- .config/mpv/update_scripts.sh
+++ .config/mpv/update_scripts.sh
@@ -1,7 +1,7 @@
 scripts=(
-    "4e6/mpv-reload:reload.lua"
-    "po5/mpv_sponsorblock:sponsorblock.lua,sponsorblock_shared"
-    "po5/thumbfast:thumbfast.lua"
+    "https://raw.githubusercontent.com/4e6/mpv-reload/master/reload.lua"
+    "https://raw.githubusercontent.com/po5/thumbfast/master/thumbfast.lua"
+    "https://github.com/hoyon/mpv-mpris/releases/latest/download/mpris.so"
 )
 
 mpv_dir="${XDG_CONFIG_HOME:-$HOME/.config}/mpv"
@@ -11,24 +11,14 @@ rm  -rfv "$mpv_dir/scripts" "$mpv_dir/script-opts" "$mpv_dir/fonts"
 mkdir -v "$mpv_dir/scripts" "$mpv_dir/script-opts"
 
 for script in "${scripts[@]}"; do
-    split=($(printf "%s" "$script" | sed "s/:/ /"))
-    contents=($(printf "%s" ${split[1]} | sed "s/,/ /g"))
-
-    split=($(printf "%s" $split | sed "s/\// /"))
-    user="$split"
-    repo="${split[1]}"
-
-    git clone -- "https://www.github.com/$user/$repo.git" "/tmp/$repo"
-    for content in "${contents[@]}"; do
-        mv -v "/tmp/$repo/$content" "$mpv_dir/scripts" 
-    done
-    rm -rfv "/tmp/$repo"
+    wget --directory-prefix "$mpv_dir/scripts/" "$script"
 done
 
-# we do uosc separately because it uses a different structure
+# sponsorblock
+git clone -- "https://www.github.com/po5/mpv_sponsorblock" "/tmp/mpv_sponsorblock"
+mv -v "/tmp/mpv_sponsorblock/sponsorblock.lua" "/tmp/mpv_sponsorblock/sponsorblock_shared/" "$mpv_dir/scripts/"
+
+# uosc
 wget -P /tmp/ https://github.com/tomasklaen/uosc/releases/latest/download/uosc.zip
 unzip -od "$mpv" /tmp/uosc.zip
 rm -fv /tmp/uosc.zip
-
-# and this because they provide the script in releases
-wget -O "$mpv_dir/scripts/mpris.so" https://github.com/hoyon/mpv-mpris/releases/latest/download/mpris.so
