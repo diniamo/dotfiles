887d68a9904536c7d1d4f7825c663c9c3c499085
diniamo <diniamo69@gmail.com>
2023-09-17 16:13:27 +0200

    add thefuck back + add qbittorent to autostart

--- .scripts/autostart.sh
+++ .scripts/autostart.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Config
 setxkbmap hu
 
+# Background
 dunst &
 if [[ -d "/sys/class/power_supply" ]]; then x-on-resize --config ~/.scripts/hdmi_toggle.sh & fi
 redshift -l 47.1625:19.5033 &
 betterlockscreen -u $WALLPAPER &
 xss-lock -- sh -c "playerctl -a pause; betterlockscreen -l dimblur" &
+
+# Applications
+qbittorrent &
