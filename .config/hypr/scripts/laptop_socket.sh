4aa2638e2daf73af8eaecbbbc010f0a346f3b790
diniamo <diniamo69@gmail.com>
2023-11-03 16:38:53 +0100

    keybind adjustments, windows are now automatically moved out of the file manager special workspace

--- .config/hypr/scripts/laptop_socket.sh
+++ .config/hypr/scripts/laptop_socket.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r event; do
-    action=$(printf '%s' "$event" | grep -oP '^.*(?=>>)')
-    details=$(printf '%s' "$event" | grep -oP '^.*>>\K.*')
+    action=${event%%>>*}
+    details=${event##*>>}
 
     case "$action" in
         monitoradded|monitorremoved)
-            if printf '%s' "$details" | grep -q '^HDMI.*'; then
+            if [[ "$details" == "HDMI*" ]]; then
                 ~/.config/hypr/scripts/hdmi_hotplug.sh
             fi
             ;;
--- .config/hypr/scripts/socket.sh
+++ .config/hypr/scripts/socket.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
-socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read -r event; do
-    action=$(printf '%s' "$event" | grep -oP '^.*(?=>>)')
-    details=$(printf '%s' "$event" | grep -oP '^.*>>\K.*')
+socat -U - UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock | while read event; do
+    action=${event%%>>*}
+    details=${event##*>>}
 
     case "$action" in
+        openwindow)
+            details=($(tr ',' '\n' <<< "$details"))
 
+            if [ "${details[1]}" = "special:fm" ]; then
+                hyprctl dispatch movetoworkspacesilent "$(hyprctl -j activeworkspace | jq -r '.id'),address:0x${details[0]}"
+            fi
     esac
 done
 
