a203135409f679953a2d11ddac878ef76eaeeecc
diniamo <diniamo69@gmail.com>
2023-11-08 21:19:52 +0100

    fullscreen.sh fixed

--- .config/hypr/scripts/fullscreen.sh
+++ .config/hypr/scripts/fullscreen.sh
@@ -1,13 +1,65 @@
 #!/bin/sh
 
-WORKSPACE_ID=$(hyprctl -j activeworkspace | jq -r '.id')
-if hyprctl -j activeworkspace | jq -e '.hasfullscreen' >/dev/null; then
-    HYPRLAND_CONF=$(cat "$HOME/.config/hypr/hyprland.conf")
-    GAPS_OUT=$(printf '%s' "$HYPRLAND_CONF" | grep -oP '(gaps_out\s*=\s*)\K(\d*)')
-    ROUNDING=$(printf '%s' "$HYPRLAND_CONF" | grep -oP '(rounding\s*=\s*)\K(\d*)')
-
-    hyprctl keyword workspace "$WORKSPACE_ID, gapsout:$GAPS_OUT,border:true,rounding:$ROUNDING"
-else
-    hyprctl keyword workspace "$WORKSPACE_ID, gapsout:0,border:false,rounding:0"
+if hyprctl -j monitors | jq -e 'any(.[]; .specialWorkspace.name != "")'; then
+    exit 0
 fi
-hyprctl dispatch fullscreen 1
+
+
+workspace_id=$(hyprctl -j activeworkspace | jq -r '.id')
+workspace_string=$(hyprctl -j activeworkspace | jq -r '.name')
+if [ "$workspace_string" != "$workspace_id" ]; then
+    workspace_string="name:$workspace_string"
+fi
+
+state_file="/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/fullscreen_state.json"
+if [ ! -f "$state_file" ]; then
+    printf '{}' > "$state_file"
+
+    original_state='{}'
+else
+    original_state=$(cat "$state_file")
+fi
+
+
+get_option() {
+    if [ ! -z "$1" ]; then
+        value=$(hyprctl -j workspacerules | jq -r ".[] | select(.workspaceString == \"$workspace_string\") | .$1")
+    fi
+
+    if [ "$value" = "null" -o -z "$value" ]; then
+        value=$(hyprctl -j getoption "$2" | jq -r ".$3")
+    fi
+
+    printf '%s' "$value"
+}
+
+get_state_option() {
+    value=$(printf '%s' "$original_state" | jq -r ".\"$workspace_id\".$1")
+
+    if [ "$value" = "null" ]; then
+        echo "$1 does not exist in the state file!" 1>&2
+        exit 1
+    fi
+
+    printf '%s' "$value"
+}
+
+if hyprctl -j activeworkspace | jq -e '.hasfullscreen' >/dev/null; then
+    hyprctl keyword workspace "$workspace_id, gapsout:$(get_state_option 'gapsOut'),border:true,rounding:$(get_state_option 'rounding')"
+    hyprctl dispatch fullscreen
+else
+    gaps_out=$(get_option 'gapsOut' 'general:gaps_out' 'int')
+    rounding=$(get_option 'rounding' 'decoration:rounding' 'int')
+    if [ "$rounding" = "true" ]; then
+        rounding=$(get_option '' 'decoration:rounding' 'int')
+    elif [ "$rounding" = "false" ]; then
+        rounding="0"
+    fi
+
+    tmp=$(mktemp)
+    printf '%s' "$original_state" | jq ".\"$workspace_id\".gapsOut = \"$gaps_out\" | .\"$workspace_id\".rounding = \"$rounding\"" > "$tmp"
+    mv "$tmp" "$state_file"
+
+    hyprctl keyword workspace "$workspace_id, gapsout:0,border:false,rounding:0"
+    hyprctl dispatch fullscreen 1
+fi
