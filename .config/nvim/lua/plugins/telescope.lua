5bcec0080c4488123f91e6038062f8cfec002f8f
diniamo <diniamo69@gmail.com>
2024-01-13 12:41:09 +0100

    more neovim zoxide integration

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -6,9 +6,23 @@ return {
         },
         init = function()
             local telescope = require("telescope")
+            local map = vim.keymap.set
 
             telescope.load_extension("zoxide")
-            vim.keymap.set("n", "<leader>z", telescope.extensions.zoxide.list)
+            map("n", "<leader>zi", telescope.extensions.zoxide.list)
+            map("n", "<leader>zz", function()
+                local keyword = vim.fn.input("Keyword: ")
+                local directory = vim.system({ "zoxide", "query", keyword }, { text = true })
+                    :wait().stdout
+                    :match("(.*)[\n\r]")
+
+                if directory == "" or directory == nil then
+                    vim.notify("No match found", vim.log.levels.ERROR)
+                else
+                    vim.notify(directory)
+                    vim.fn.chdir(directory)
+                end
+            end)
         end,
         keys = {
             { "<leader>z" },
